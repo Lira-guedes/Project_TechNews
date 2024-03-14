f408f122ff7c4e8fe0e8188a1adfd3ee0795aef6
Lira Guedes <liracrg@gmail.com>
2024-03-14 14:09:52 -0300

    feat: Cria a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,7 +1,20 @@
+from tech_news.database import db
+
 # Requisito 7
 def search_by_title(title):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        news = db.news.find(
+            {"title":
+             {"$regex": title, "$options": "i"}
+            },
+            projection=["title", "url"],
+        )
+
+        list_news = [(new["title"], new["url"]) for new in news]
+        return list_news
+
+    except Exception as error:
+        raise error
 
 
 # Requisito 8
